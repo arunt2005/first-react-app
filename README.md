db3e9ae3582ebd19bc511a3e7fdb3718c07e6ae4
arunt <arunt2005@gmail.com>
2025-11-10 20:08:44 +0530

    Initial React app

---
+++ README.md
@@ -0,0 +1,2 @@
+# First React App
+This is a sample React app configured for GitHub Pages deployment.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 Welcome to My First React App</h1>
+      <p>This React app is deployed on <b>GitHub Pages</b>!</p>
+      <p>Developed by <b>Arun</b> 💻</p>
+    </div>
+  );
+}
+
+export default App;
