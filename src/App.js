80da939f6ecde64e32aecedd10753cfeb12ac7c3
arunt <arunt2005@gmail.com>
2025-11-10 20:25:10 +0530

    text change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>🚀 Welcome to My First React App</h1>
+      <h1>🚀 Welcome to My First React App - Hello World</h1>
       <p>This React app is deployed on <b>GitHub Pages</b>!</p>
       <p>Developed by <b>Arun</b> 💻</p>
     </div>
